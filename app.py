876e1618b47fe12ccf313eb85c553fa4abd3f3a2
Felipe Kucharski <62360343+FaKeBx@users.noreply.github.com>
2023-06-13 12:31:24 -0300

    Update app.py

--- app.py
+++ app.py
@@ -21,4 +21,4 @@ def message_handler(msg):
     send(messages)
 
 if __name__ == "__main__":
-    io.run(app,host="0.0.0.0",port=5000, debug=True)
+    io.run(app, host='0.0.0.0', port=5000, debug=True)
