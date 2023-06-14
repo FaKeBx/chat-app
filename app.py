ba255f9b47e071029b18c58fa70d564cbc2448c6
Felipe Kucharski <62360343+FaKeBx@users.noreply.github.com>
2023-06-13 23:03:18 -0300

    pqp

--- app.py
+++ app.py
@@ -37,4 +37,4 @@ def handle_message(message):
     emit('broadcast', message, broadcast=True, include_self=False)
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=int(os.getenv("PORT")))
+    socketio.run(app, host='0.0.0.0', port=int(os.getenv("PORT")), allow_unsafe_werkzeug=True)
